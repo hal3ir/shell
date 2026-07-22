55c9b225a367d71a287352b1f68fc8f4fb48c986
Anlan Hong <hal3ir@outlook.com>
2026-07-22 19:35:52 -0400

    completed assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -60,8 +60,10 @@ cp ./data/raw/*user*.log ./data/processed/user_logs/
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-find ./data/processed -type f > ./data/inventory.txt
-
+touch ./data/inventory.txt
+ls ./data/processed/server_logs >> ./data/inventory.txt
+ls ./data/processed/user_logs >> ./data/inventory.txt
+ls ./data/processed/event_logs >> ./data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
